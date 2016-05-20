5e2ec6787ff5bc48bbd2f17da30e73fe3b0e9e2a
Taylor Jones <taylorjonessoftware@gmail.com>
2016-05-20 11:17:16 -0400

    more posts methods

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use dotenv::dotenv;
 use std::env;
+use self::models::{Post, NewPost};
 
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
@@ -21,4 +22,18 @@ pub fn establish_connection() -> PgConnection {
         .expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
+}
+
+pub fn create_post<'a>(conn: &PgConnection, title: &'a str, body: &'a str, user_id: i32) -> Post {
+    use schema::posts;
+
+    let new_post = NewPost {
+        title: title,
+        body: body,
+        user_id: user_id,
+    };
+
+    diesel::insert(&new_post).into(posts::table)
+        .get_result(conn)
+        .expect("Error saving new post")
 }
--- src/models.rs
+++ src/models.rs
@@ -5,3 +5,12 @@ pub struct Post {
     pub title: String,
     pub body: String,
 }
+
+use super::schema::posts;
+
+#[insertable_into(posts)]
+pub struct NewPost<'a> {
+    pub title: &'a str,
+    pub body: &'a str,
+    pub user_id: i32,
+}
