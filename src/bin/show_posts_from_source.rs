4f2720ead58efcd4675a1e6ef4086897946bd27f
Taylor Jones <taylorjonessoftware@gmail.com>
2016-05-20 15:50:47 -0400

    stopping for now

--- src/bin/show_posts_from_source.rs
+++ src/bin/show_posts_from_source.rs
@@ -1,16 +1,22 @@
 extern crate rustweb;
-extern crate diesel;
+// extern crate diesel;
 extern crate hyper;
+// extern crate rustc_serialize;
+
 
 // use self::rustweb::*;
 // use self::rustweb::models::*;
 // use self::diesel::prelude::*;
+
 use hyper::{Client};
 use std::io::Read;
-
+// use rustc_serialize::Decodable;
+// use rustc_serialize::Encodable;
+// use rustc_serialize::json::{self, Encoder};
 
 fn main() {
-    println!("{:?}", get_content("http://jsonplaceholder.typicode.com/posts"));
+    let response = get_content("http://jsonplaceholder.typicode.com/posts/1").unwrap();
+    println!("{:?}", response);
 }
 
 fn get_content(url: &str) -> hyper::Result<String> {
